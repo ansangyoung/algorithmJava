c6e600c4dba9d1ab3a677560dd9799bbf99f0717
ansangyoung <ssffwert@naver.com>
2021-05-23 20:05:03 +0900

    leetcode dp 추가

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ stack/stack42586 추가
 2021.05.15  
 list/list238 추가  
 string/string1247 추가  
+
+2021.05.23  
+dp/dp1638 추가  
---
+++ src/dp/dp1638.java
@@ -0,0 +1,71 @@
+// https://leetcode.com/problems/count-substrings-that-differ-by-one-character/
+// 1638. Count Substrings That Differ by One Character
+// https://kickstart.best/1638-count-substrings-that-differ-by-one-character/ 참고
+package dp;
+
+class Solution {
+    int countSubstrings(String s, String t) {
+        int[][] dp1 = new int[105][105];
+        int[][] dp2 = new int[105][105];
+        
+        int sLen = s.length();
+        int tLen = t.length();
+        s = "#" + s + "#";
+        t = "#" + t + "#";
+        
+        // left to right
+        for(int i = 1; i <= sLen; i++) {
+            for(int j = 1; j <= tLen; j++) {
+                if(s.charAt(i) == t.charAt(j)) {
+                	dp1[i][j] = dp1[i-1][j-1] + 1;
+                } else {
+                	dp1[i][j] = 0;
+                }
+            }
+        }
+        
+        // right to left
+        for(int i = sLen; i >= 1; i--) {
+            for(int j = tLen; j >= 1; j--) {
+                if(s.charAt(i) == t.charAt(j)) {
+                	dp2[i][j] = dp2[i+1][j+1] + 1;
+                } else {
+                	dp2[i][j] = 0;
+                }
+            }
+        }
+        
+        int ret = 0;
+        for(int i = 1; i <= sLen; i++) {
+            for(int j = 1; j <= tLen; j++) {
+                if(s.charAt(i) != t.charAt(j)) {
+                    ret += (dp1[i-1][j-1] + 1) * (dp2[i+1][j+1] + 1);
+                }
+            }
+        }
+//System.out.println("ret:" + ret);
+        return ret;
+    }
+}
+
+public class dp1638 {
+	public static void main(String[] args) {
+		String s = "aba";
+		String t = "baba";
+
+		Solution soultion = new Solution();
+		soultion.countSubstrings(s, t); // 6
+
+		s = "ab";
+		t = "bb";
+		soultion.countSubstrings(s, t); // 3
+
+		s = "a";
+		t = "a";
+		soultion.countSubstrings(s, t); // 0
+
+		s = "abe";
+		t = "bbc";
+		soultion.countSubstrings(s, t); // 10
+	}
+}
