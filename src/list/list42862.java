e139b316fc262babb81afbaa42e358d65b8037c8
ansangyoung <ssffwert@naver.com>
2021-05-29 18:38:30 +0900

    다른사람 풀이 참조하여 효율성있는 코드로 수정

--- src/list/list42862.java
+++ src/list/list42862.java
@@ -1,68 +1,39 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42862
 // 체육복
+// https://programmers.co.kr/learn/courses/30/lessons/42862/solution_groups?language=java 참조
 package list;
 import java.util.*;
 
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int nMin = 1;
-        int nMax = n;
-        int extraNum = 30;
-        int[] studentTmp = new int[nMax + 1];
-        
-        for(int i = 1; i <= nMax; i++) {
-        	studentTmp[i] = 1;
+        int[] people = new int[n + 2];
+        int answer = n;
+
+        for (int lostVal : lost) {
+            people[lostVal]--;
         }
         
-        for(int lostVal : lost) {
-        	studentTmp[lostVal]--;
+        for (int reserveVal : reserve) {
+            people[reserveVal]++;
         }
         
-        for(int reserveVal : reserve) {
-        	if(studentTmp[reserveVal] == 0) {
-        		studentTmp[reserveVal] = 1;
-        	} else {
-        		studentTmp[reserveVal] = extraNum;
-        	}
+        int peopleLen = people.length;
+        for (int i = 1; i < peopleLen; i++) {
+            if(people[i] == -1) {
+                if(people[i - 1] == 1) {
+                    people[i]++;
+                    people[i - 1]--;
+                } else if(people[i+1] == 1) {
+                    people[i]++;
+                    people[i + 1]--;
+                } else {
+                    answer--;
+                }
+            }
         }
-        
-        for(int lostVal : lost) {
-        	if(studentTmp[lostVal] == 0) {
-	        	if(lostVal != nMax && studentTmp[lostVal + 1] > 2) {
-	        		studentTmp[lostVal]++;
-	        		studentTmp[lostVal + 1]--;
-	        	}
-	        	
-	        	if(lostVal != nMin && studentTmp[lostVal - 1] > 2) {
-	        		studentTmp[lostVal]++;
-	        		studentTmp[lostVal - 1]--;
-	        	}
-        	}
-        }
-        
-        for(int i = 1; i <= nMax; i++) {
-//System.out.println(i + ":" + studentTmp[i]);
-        	if(studentTmp[i] == extraNum - 2) {
-        		if(studentTmp[i - 1] == 1 && studentTmp[i + 1] == 1) {
-        			answer -= 1;
-        		}
-        	}
-        	
-        	if(studentTmp[i] > 0) {
-        		if(studentTmp[i] > 2) {
-        			answer += 1;
-        		} else {
-        			if(studentTmp[i] == 2) {
-        				answer -= 1;
-        			}
-        			answer += studentTmp[i];
-        		}
-        	}
-        }
-//System.out.println("answer:" + answer);
-//System.out.println("");
-        return answer;
+System.out.println("answer:" + answer);
+		return answer;
     }
 }
 
@@ -99,5 +70,15 @@ public class list42862 {
 		lost = new int[] {2, 4 };
 		reserve = new int[] {3, 5};
 		soultion.solution(n, lost, reserve); // 5
+		
+		n = 5;
+		lost = new int[] {1, 3, 5};
+		reserve = new int[] {2, 4};
+		soultion.solution(n, lost, reserve); // 4
+
+		n = 6;
+		lost = new int[] {1, 3, 5};
+		reserve = new int[] {2, 4, 6};
+		soultion.solution(n, lost, reserve); // 6
 	}
 }
