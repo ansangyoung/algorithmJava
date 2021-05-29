8ecee1c67b079ef5fed383357c353c9f83cd47cb
ansangyoung <ssffwert@naver.com>
2021-05-29 13:55:30 +0900

    leetCode 648. Replace Words 추가

---
+++ src/string/string648.java
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/replace-words/
+// 648. Replace Words
+package string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String res = "";
+        String[] sentenceLst = sentence.split(" ");
+        
+        for(String dicVal : dictionary) {
+        	int sentenceLen = sentenceLst.length;
+        	for(int i = 0; i < sentenceLen; i++) {
+        		String sentenceVal = sentenceLst[i];
+        		if(sentenceVal.startsWith(dicVal)) {
+        			sentenceLst[i] = dicVal;
+        		}
+        	}
+        }
+        
+        for(String sentenceVal : sentenceLst) {
+        	res += sentenceVal + " ";
+        }
+        res = res.substring(0, res.length() - 1);
+//System.out.println("res:" + res);
+        return res;
+    }
+}
+
+public class string648 {
+	public static void main(String[] args) {
+		List<String> dictionary = Arrays.asList("cat", "bat", "rat");
+		String sentence = "the cattle was rattled by the battery";
+		
+		Solution soultion = new Solution();
+		soultion.replaceWords(dictionary, sentence);	// "the cat was rat by the bat"
+		
+		dictionary = Arrays.asList("a", "b", "c");
+		sentence = "aadsfasf absbs bbab cadsfafs";
+		soultion.replaceWords(dictionary, sentence);	// "a a b c"
+		
+		dictionary = Arrays.asList("a", "aa", "aaa", "aaaa");
+		sentence = "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa";
+		soultion.replaceWords(dictionary, sentence);	// "a a a a a a a a bbb baba a"
+		
+		dictionary = Arrays.asList("catt","cat","bat","rat");
+		sentence = "the cattle was rattled by the battery";
+		soultion.replaceWords(dictionary, sentence);	// "the cat was rat by the bat"
+		
+		dictionary = Arrays.asList("ac","ab");
+		sentence = "it is abnormal that this solution is accepted";
+		soultion.replaceWords(dictionary, sentence);	// "it is ab that this solution is ac"
+	}
+}
