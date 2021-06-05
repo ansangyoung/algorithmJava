0c094c9328a527a3b0c754f121fbf8ad8c3531d8
ansangyoung <ssffwert@naver.com>
2021-06-05 10:58:58 +0900

    leetcode 230 tree 추가

---
+++ src/tree/tree230.java
@@ -0,0 +1,77 @@
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// 230. Kth Smallest Element in a BST
+package tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+class Solution {
+	public void treeSearch(TreeNode root, ArrayList<Integer> list) {
+    	if(root != null) {
+    		list.add(root.val);
+    		if(root.left != null) {
+    			treeSearch(root.left, list);
+    		}
+    		if(root.right != null) {
+    			treeSearch(root.right, list);
+    		}
+    	}
+	}
+	
+    public int kthSmallest(TreeNode root, int k) {
+        int res = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        treeSearch(root, list);
+        Collections.sort(list);
+        res = list.get(k - 1);
+//System.out.println("res:" + res);
+		return res;
+    }
+}
+
+public class tree230 {
+	public static void main(String[] args) {
+		TreeNode root= new TreeNode(3);
+		//root = [3, 1, 4, null, 2]
+		root.left = new TreeNode(1);
+		root.left.left = null;
+		root.left.right = new TreeNode(2);
+		root.right = new TreeNode(4);
+		
+		int k = 1;
+		Solution soultion = new Solution();
+		soultion.kthSmallest(root, k); // 1
+		
+		
+		root = null;
+		root= new TreeNode(5);
+		root.left = new TreeNode(3);
+		root.left.left = new TreeNode(2);
+		root.left.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(4);
+		root.right = new TreeNode(6);
+		
+		k = 3;
+		soultion.kthSmallest(root, k); // 3
+	}
+}
