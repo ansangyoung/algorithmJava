88b0cae6760aa265749f7eb8b006735e1e6aaf0f
ansangyoung <ssffwert@naver.com>
2021-06-05 10:36:11 +0900

    leetcode 763 추가

---
+++ src/string/string763.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/partition-labels/
+// 763. Partition Labels
+package string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+    	List<Integer> res = new ArrayList<Integer>();
+    	
+    	int sLen = s.length();
+    	int i = 0;
+    	for(; i < sLen;) {
+    		int resVal = i;
+    		char chkLetter = s.charAt(i);
+    		int lastIdx = s.lastIndexOf(chkLetter);
+    		for(int j = i + 1; j < lastIdx; j++) {
+    			char chkPartLetter = s.charAt(j);
+    			int lastPartIdx = s.lastIndexOf(chkPartLetter);
+    			if(lastIdx < lastPartIdx) {
+    				lastIdx = lastPartIdx;
+    			}
+    		}
+    		i = lastIdx + 1;
+    		res.add(i - resVal);
+    	}
+    	return res;
+    }
+}
+
+public class string763 {
+	public static void main(String[] args) {
+		String s = "ababcbacadefegdehijhklij";
+		
+		Solution soultion = new Solution();
+		soultion.partitionLabels(s);	// [9, 7, 8]
+		
+		s = "caedbdedda";
+		soultion.partitionLabels(s);	// [1, 9]
+	}
+}
