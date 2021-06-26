1718c35e1d907e4fb4f7195a6a02e34a347f867c
ansangyoung <ssffwert@naver.com>
2021-06-26 13:56:32 +0900

    leetcode 139. Word Break 추가

---
+++ src/main/set/set139.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/word-break/
+// 139. Word Break
+// https://leetcode.com/problems/count-number-of-nice-subarrays/discuss/419378/JavaC%2B%2BPython-Sliding-Window-atMost(K)-atMost(K-1) 참고
+package main.set;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class set139 {
+	public static boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> wordDictSet = new HashSet<>(wordDict);
+		int sLen = s.length();
+		boolean[] dp = new boolean[sLen + 1];
+		dp[0] = true;
+		for (int i = 1; i <= sLen; i++) {
+			for (int j = 0; j < i; j++) {
+				if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+//System.out.println("dp[sLen]:" + dp[sLen]);
+		return dp[sLen];
+	}
+
+	public static void main(String[] args) {
+		String s = "leetcode";
+		List<String> wordDict = new ArrayList<String>();
+		wordDict.add("leet");
+		wordDict.add("code");
+		wordBreak(s, wordDict);
+	}
+}
