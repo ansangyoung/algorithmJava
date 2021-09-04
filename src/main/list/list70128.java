a380a6c386d547b8913e66c0901d761b60d9c5fc
ansangyoung <ssffwert@naver.com>
2021-09-04 15:22:19 +0900

    패키지 및 인코딩 수정

---
+++ src/main/list/list70128.java
@@ -0,0 +1,29 @@
+// https://programmers.co.kr/learn/courses/30/lessons/70128
+// 내적
+package main.list;
+import java.util.*;
+class Solution9 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        int aLen = a.length;
+        for(int i = 0; i < aLen; i++) {
+        	answer += a[i] * b[i];
+        }
+System.out.println("answer:" + answer);
+        return answer;
+    }
+}
+
+public class list70128 {
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4};
+		int[] b = {-3, -1, 0, 2};
+		
+		Solution9 soultion = new Solution9();
+		soultion.solution(a, b); // 3
+		
+		a = new int[] {-1, 0, 1};
+		b = new int[] {1, 0, -1};
+		soultion.solution(a, b); // -2
+	}
+}
