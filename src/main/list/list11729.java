c2935873123dab232b3ada942832b23bf04016cb
ansangyoung <ssffwert@naver.com>
2021-09-06 21:30:10 +0900

    하노이 탑 이동 순서, 코드개선

--- src/main/list/list11729.java
+++ src/main/list/list11729.java
@@ -1,4 +1,5 @@
 // https://st-lab.tistory.com/96 참고
+// 하노이 탑 이동 순서
 package main.list;
 import java.util.Scanner;
 
@@ -9,31 +10,27 @@ public class list11729 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+		int n = in.nextInt();
+		int resultCnt = (int)Math.pow(2, n) - 1;
+		sb.append(resultCnt).append('\n');
 		
-		Hanoi(N, 1, 2, 3);
+		Hanoi(n, 1, 2, 3);
 		System.out.println(sb);
 	}
 
-	/*
-	 * N: 원판의 개수 
-	 * start: 출발지
-	 * to: 목적지
-	 */
-	
-	public static void Hanoi(int N, int start, int mid, int to) {
-		if (N == 1) {
+	// n: 원판의 개수, start: 출발지, to: 목적지
+	public static void Hanoi(int n, int start, int mid, int to) {
+		if (n == 1) {
 			sb.append(start + " " + to + "\n");
 			return;
 		}
 		// STEP 1 : N-1개를 A에서 B로 이동
-		Hanoi(N - 1, start, to, mid);
+		Hanoi(n - 1, start, to, mid);
 
 		// STEP 2 : 1개를 A에서 C로 이동
 		sb.append(start + " " + to + "\n");
 
 		// STEP 3 : N-1개를 B에서 C로 이동
-		Hanoi(N - 1, mid, start, to);
+		Hanoi(n - 1, mid, start, to);
 	}
 }
